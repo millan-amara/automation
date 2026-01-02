090a352ad9b36bc5d24841078b8d6387b0f8a015
Hughes <onyangohughes8611@gmail.com>
2026-01-02 21:44:40 +0300

    fix link import

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,5 +1,5 @@
 // src/components/Home/Hero.jsx
-import { ArrowRight, Link, Play } from 'lucide-react'
+import { ArrowRight, Play } from 'lucide-react'
 import Button from '../UI/Button'
 import { Link } from 'react-router-dom'
 
